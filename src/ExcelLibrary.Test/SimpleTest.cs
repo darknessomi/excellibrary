aef699aaf0159acc7720444d4f12e3b9b5147dc2
jetcat <jetcat@ac858716-e6e7-11dd-85a8-df16bb8a7048>
2009-01-25 06:26:49 +0000

    Include some more test cases

--- src/ExcelLibrary.Test/SimpleTest.cs
+++ src/ExcelLibrary.Test/SimpleTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Text;
 using NUnit.Framework;
 using QiHe.Office.Excel;
 
@@ -28,6 +30,106 @@ namespace ExcelLibrary.Test
                 Assert.AreEqual("Test1", worksheet.Name);
                 Assert.AreEqual(100, worksheet.Cells[0, 1].Value);
                 Assert.AreEqual("Test String", worksheet.Cells[2, 0].Value);
+            }
+        }
+
+        [Test]
+        public void SimpleMultipleSheetTest()
+        {
+            int worksheetToCreate = 10;
+
+            string tempFilePath = Path.GetTempFileName();
+            {
+                Workbook workbook = new Workbook();
+                for (int i = 0; i < worksheetToCreate; i++)
+                {
+                    Worksheet worksheetWrite2 = new Worksheet(String.Format("Sheet {0}", i));
+                    workbook.Worksheets.Add(worksheetWrite2);
+                }
+
+                workbook.Save(tempFilePath);
+            }
+
+            {
+                Workbook workbook = Workbook.Open(tempFilePath);
+
+                Assert.AreEqual(worksheetToCreate, workbook.Worksheets.Count);
+                for (int i = 0; i < worksheetToCreate; i++)
+                {
+                    Assert.AreEqual(String.Format("Sheet {0}", i), workbook.Worksheets[i].Name);
+                }
+            }
+        }
+
+        [Test]
+        public void WriteLongTextTest()
+        {
+            int longTextLength = 50000;
+
+            StringBuilder builder = new StringBuilder(longTextLength);
+            for (int i = 0; i < longTextLength; i++)
+                builder.Append('A');
+
+            string longText = builder.ToString();
+
+            string tempFilePath = Path.GetTempFileName();
+            {
+                Workbook workbook = new Workbook();
+                Worksheet worksheet = new Worksheet("Test");
+                worksheet.Cells[0, 0] = new Cell(longText);
+
+                workbook.Worksheets.Add(worksheet);
+                workbook.Save(tempFilePath);
+            }
+
+            {
+                Workbook workbook = Workbook.Open(tempFilePath);
+                Assert.AreEqual(longText, workbook.Worksheets[0].Cells[0, 0].Value);
+            }
+        }
+
+        [Test]
+        public void WriteMultipleCellTest()
+        {
+            int worksheetToWrite = 30;
+            int rowToWrite = 200;
+            int columnToWrite = 15;
+
+            string tempFilePath = Path.GetTempFileName();
+            {
+                int start = Environment.TickCount;
+
+                Workbook workbook = new Workbook();
+                for (int sheet = 0; sheet < worksheetToWrite; sheet++)
+                {
+                    Worksheet worksheet = new Worksheet(String.Format("Sheet {0}", sheet));
+
+                    for (int row = 0; row < rowToWrite; row++)
+                        for (int column = 0; column < columnToWrite; column++)
+                            worksheet.Cells[row, column] = new Cell(String.Format("{0}{1}", row, column));
+
+                    workbook.Worksheets.Add(worksheet);
+                }
+                workbook.Save(tempFilePath);
+
+                int end = Environment.TickCount;
+                Console.WriteLine(String.Format("Write tick count: {0}", end - start));
+            }
+
+            {
+                int start = Environment.TickCount;
+                
+                Workbook workbook = Workbook.Open(tempFilePath);
+                for (int sheet = 0; sheet < worksheetToWrite; sheet++)
+                    for (int row = 0; row < rowToWrite; row++)
+                        for (int column = 0; column < columnToWrite; column++)
+                        {
+                            Assert.AreEqual(String.Format("{0}{1}", row, column), workbook.Worksheets[sheet].Cells[row, column].Value);
+                        }
+
+                int end = Environment.TickCount;
+
+                Console.WriteLine(String.Format("Read tick count: {0}", end - start));
             }
         }
     }
