d94f09f1a06a6708f70f95b0a4d4e699e80dc2fa
jetcat <jetcat@ac858716-e6e7-11dd-85a8-df16bb8a7048>
2009-02-13 00:36:11 +0000

    Include LGPL license into solution and also include website address to assembly info

--- src/ExcelLibrary.WinForm/Properties/AssemblyInfo.cs
+++ src/ExcelLibrary.WinForm/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExcelLibrary.WinForm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("http://code.google.com/p/excellibrary/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExcelLibrary.WinForm")]
